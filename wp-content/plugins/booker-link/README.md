66f3c825b33d565f0ca22511be58eb990f0ddff2
Edd Twilbeck <etwilbeck@marketingincolor.com>
2019-08-15 09:35:34 -0500

    preliminary plugin setup

---
+++ wp-content/plugins/booker-link/README.md
@@ -0,0 +1,2 @@
+# booker-link
+This WordPress plugin provides a way for franchises using Booker.com to include the ability to make appointments via their website
---
+++ wp-content/plugins/booker-link/booker-link.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Booker.com Link
+Plugin URI: http://zeepress.com/plugins/bookerlink
+Description: Provides a link to Booker.com Salon Management.
+Version: 0.1
+Author: Marketing In Color
+Author URI: http://marketingincolor.com
+Text Domain: booker-link
+Domain Path: /lang/
+*/
