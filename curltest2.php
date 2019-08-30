ab8139d26be545a7fda2b7efd2136eec8bda93ea
Edd Twilbeck <etwilbeck@marketingincolor.com>
2019-08-30 10:38:00 -0500

    test2 working pre bookertoken as class

--- curltest2.php
+++ curltest2.php
@@ -1,16 +1,19 @@
 <?php
+//
+// ORGINAL CODES AND KEYS STORED COMMENTED BELOW
+//
+//$OASKey = 'fe2de837b6624c91a93e36350743e223';
+//$grantstring = "grant_type=client_credentials&client_id=wjfobeqQUIsQ&client_secret=7HcPPOhErNE1&scope=customer";
 
+$client_id = 'wjfobeqQUIsQ';
+$client_secret = '7HcPPOhErNE1';
 
-$OASKey = 'fe2de837b6624c91a93e36350743e223';
-$grantstring = "grant_type=client_credentials&client_id=wjfobeqQUIsQ&client_secret=7HcPPOhErNE1&scope=customer";
-
-
-function createBookerToken() {
+function createBookerToken($cid, $csecret) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, 'https://api-staging.booker.com/v5/auth/connect/token');
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    //curl_setopt($ch, CURLOPT_POSTFIELDS, "grant_type=client_credentials&client_id=wjfobeqQUIsQ&client_secret=7HcPPOhErNE1&scope=customer");
-    curl_setopt($ch, CURLOPT_POSTFIELDS, sprintf("grant_type=client_credentials&client_id=wjfobeqQUIsQ&client_secret=7HcPPOhErNE1&scope=customer", $OASKey) );
+    curl_setopt($ch, CURLOPT_POSTFIELDS, sprintf("grant_type=client_credentials&client_id=%s&client_secret=%s&scope=customer", $cid, $csecret) );
+
 
     //if ($_SERVER['SERVER_NAME'] == 'localhost') {
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
@@ -30,70 +33,29 @@ function createBookerToken() {
     return $result;
 }
 
-$output = createBookerToken();
-$token = json_decode($output);
-    //print $token->{'access_token'};
-
-//global $sessiontoken;
-
-$sessiontoken = $token->access_token;
-//print $sessiontoken;
-//echo "<hr>";
+$tokenoutput = createBookerToken($client_id, $client_secret);
+$token = json_decode($tokenoutput);
+$sessiontoken = $token->access_token; //This value, once generated, is good for 30 minutes (1800 seconds)
 
 
 
-
-class sortbyCLASS {
+class sortLocationsBy {
     public $SortBy = 'Name';
     public $SortDirection = 0;
 }
 
-class locationcurlCLASS {
-    private $classtoken;
-    //public function __construct($classtoken) {
-    //    $this->classtoken = $classtoken;
-    //}
-    
-
-    public $UsePaging = true;
-    public $PageNumber = 1;
-    public $PageSize = 20;
-    public $access_token = "1234";
-    //public $access_token = $classtoken;
-
-    public $SortBy = array();
-    public function __construct(){
-        array_push($this->SortBy, new sortbyCLASS);
-    }
-}
-
-//$dothisthing = new locationcurlCLASS($sessiontoken);
-//
-$dothisthing = new locationcurlCLASS($sessiontoken);
-//echo json_encode( $dothisthing );
-
-//echo json_encode( new locationcurlCLASS() );
-//echo "<hr><hr>";
-
-
-
-
-
-
-//--------------------Trying another option, and it WORKS!!!!!!!!    
-//--------------------Trying another option, and it WORKS!!!!!!!!                                                                                        
-class Please {
+class findLocations {
     public $UsePaging = "";
     public $PageNumber = "";
     public $PageSize = "";
 
     public $SortBy = array();
     public function __construct(){
-        array_push($this->SortBy, new sortbyCLASS);
+        array_push($this->SortBy, new sortLocationsBy);
     }
     public $access_token = "";
 }
-$p = new Please();
+$p = new FindLocations();
 $p->UsePaging = true;
 $p->PageNumber = 1;
 $p->PageSize = 20;
@@ -108,40 +70,11 @@ echo "<hr><hr><hr>";
 
 
 
-
-
-
-
-function findBookerLocation($validtoken) {
+function findBookerLocation($stringToPost) {
     $chloc = curl_init();
     curl_setopt($chloc, CURLOPT_URL, 'https://api-staging.booker.com/v4.1/customer/locations');
     curl_setopt($chloc, CURLOPT_RETURNTRANSFER, 1);
-
-
-
-// ---------------------------and BOOM goes the dynamite - this is the WORKING code, but fixing the multi-array now
-    $new_array = array(
-        'UsePaging' => true,
-        'PageNumber' => 1,
-        'PageSize' => 20,
-        //'SortBy' => array(
-        //    'SortBy' => 'Name',
-        //    'SortDirection' => 0
-        //),
-        //'access_token' => $validtoken
-    );
-    //$make_it_json = json_encode($new_array, JSON_FORCE_OBJECT);
-    //echo $make_it_json;
-    //echo "<hr>";
-        //curl_setopt($chloc, CURLOPT_POSTFIELDS, json_encode($new_array, JSON_FORCE_OBJECT) );
-    //curl_setopt($chloc, CURLOPT_POSTFIELDS, $make_it_json );
-
-
-
-
-    curl_setopt($chloc, CURLOPT_POSTFIELDS, $validtoken );
-
-
+    curl_setopt($chloc, CURLOPT_POSTFIELDS, $stringToPost );
 
     //if ($_SERVER['SERVER_NAME'] == 'localhost') {
     curl_setopt($chloc, CURLOPT_SSL_VERIFYHOST, false);
@@ -161,7 +94,6 @@ function findBookerLocation($validtoken) {
     return $locresult;
 }
 
-//$findlocation = findBookerLocation();
 $findlocation = findBookerLocation($pstring);
 //$findlocation = findBookerLocation($sessiontoken);
 
